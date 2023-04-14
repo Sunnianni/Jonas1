6aab764b9785227512efef90fbc7de6d5ce44dc3
Sunnianni <119662362+Sunnianni@users.noreply.github.com>
2023-04-14 22:35:12 +0200

    Add files via upload

---
+++ dot.js
@@ -0,0 +1,83 @@
+var data = {
+    canvas: null,
+    ctx: null,
+    clickedDot: null,
+    dots: [{x: 250, y: 300}, {x: 350, y: 400}, {x: 500, y: 200}, {x: 1000, y: 550}, {x: 400, y: 600}, {x: 600, y: 300}]
+};
+
+function circleCollision (c1, c2) {
+    var a = c1.r + c2.r,
+        x = c1.x - c2.x,
+        y = c1.y - c2.y;
+
+    if ( a > Math.sqrt( (x*x) + (y*y) ) ) return true;
+    else return false;
+}
+
+function prepCanvas () {
+    var res = window.devicePixelRatio || 1,
+        scale = 1 / res;
+    data.canvas = document.getElementById('dots');
+    data.ctx = data.canvas.getContext('2d');
+    
+    data.canvas.width = window.innerWidth * res;
+    data.canvas.height = window.innerHeight * res;
+    data.canvas.style.width = window.innerWidth + 'px';
+    data.canvas.style.height = window.innerHeight + 'px';
+    
+    data.ctx.scale(res, res);
+    
+    data.canvas.addEventListener('mousedown', function (e) {
+        checkForDot(e);
+    });
+}
+
+function drawDots () {
+    var i = 0;
+    for (; i < data.dots.length; i++) {
+        var d = data.dots[i];
+        data.ctx.beginPath();
+        data.ctx.arc(d.x, d.y, 10, 0, 2*Math.PI);
+        data.ctx.fillStyle = 'black';
+        data.ctx.fill();
+        data.ctx.closePath();
+    }
+}
+function drawLine (toDot) {
+    data.ctx.beginPath();
+    data.ctx.moveTo(data.clickedDot.x, data.clickedDot.y);
+    data.ctx.lineTo(toDot.x, toDot.y);
+    data.ctx.lineWidth = 3;
+    data.ctx.strokeStyle = 'black';
+    data.ctx.stroke();
+    data.ctx.closePath();
+}
+
+function checkForDot (e) {
+    var i = 0, col = null;
+    for (; i < data.dots.length; i++) {
+        var d = data.dots[i],
+            c1 = {x: d.x, y: d.y, r: 10},
+            c2 = {x: e.pageX, y: e.pageY, r: 10};
+        if (circleCollision(c1, c2)) col = d;
+    }
+    if (col !== null) {
+        if (data.clickedDot !== null) drawLine(col);
+        data.clickedDot = col;
+    } else data.clickedDot = null;
+}
+
+prepCanvas();
+drawDots();
+
+
+
+
+
+
+
+
+
+
+
+
